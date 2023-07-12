96d4992d20b506b985a42ef2a4bb0b3c91ae0286
Nguyen Nhat Minh <52833936+Doki064@users.noreply.github.com>
2023-07-12 17:32:34 +0700

    Update main.py

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -110,7 +110,9 @@ def index():
 
             # Insert vote result into DB
             vote = request.form['vote']
-            r.incr(vote,1)
+            with tracer.span(name=f"vote {vote} clicked") as span:
+                r.incr(vote, 1)
+                logger.warning(f"{vote} voted")
 
             # Get current values
             vote1 = r.get(button1).decode('utf-8')
